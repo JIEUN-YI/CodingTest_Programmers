a92fe813d46d2f47ca486f54a687075ca5232a87
YIJEUN <yijeun.dev@gmail.com>
2024-08-06 18:06:47 +0900

    Day2_두 분수의 덧셈 풀이 추가

--- 20240805_Day2/Program.cs
+++ 20240805_Day2/Program.cs
@@ -49,20 +49,43 @@
         /// </summary>
         /// <param name="num1"></param>
         /// <param name="num2"></param>
-        public static void FountainAdd(int number1, int denom1, int number2, int denom2)
+        public static int[] FountainAdd(int number1, int denom1, int number2, int denom2)
         {
             int[] fountainArray = new int[2];
-            for(int i = 1; ; i++)
+            int comdenom = denom1 * denom2; //공통의 분모
+            int comnum = number1 * denom2 + number2 * denom1;
+            //첫번째 분수의 분자 통분 + 두번째 분수의 분자 통분 = 공통의 분자
+
+            int comdivisor = Getgcd(comnum, comdenom);
+            fountainArray[0] = comnum / comdivisor; //통분 후 약분한 분자
+            fountainArray[1] = comdenom / comdivisor;// 통분 후 약분한 분모
+
+            return fountainArray;
+        }
+        public static int Getgcd(int num1, int num2)//두 수의 최소공약수를 구하는 함수
+        {
+            // 두 수의 크기를 비교
+            if (num1 < num2) //num1의 수가 작은 경우 변경
             {
-                
+                int temp = num1;
+                num1= num2;
+                num2= temp;
             }
-           
+
+            if(num1 % num2 == 0)// 두 수를 나눈 나머지가 0이면
+            {
+                return num2; //나누는 수가 최대공약수
+            }
+            else
+            {
+                return Getgcd(num2, num1 % num2);//최대공약수가 나올때까지 재귀함수로 반복
+            }
         }
 
 
         static void Main(string[] args)
         {
-            FountainAdd(1, 2, 3, 4);
+            
         }
     }
 }
